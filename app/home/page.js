1a859295ee6201fe77d29da37705a723dd6a1e98
itzshakeeb <itzshakeeb@gmail.com>
2023-03-28 14:30:49 +0600

    added Hosting section

--- app/home/page.js
+++ app/home/page.js
@@ -1,6 +1,7 @@
 import Brand from "@/components/brand/Brand";
 import Experts from "@/components/experts/Experts";
 import Hero from "@/components/hero/Hero";
+import Hosting from "@/components/hosting/Hosting";
 import Miners from "@/components/miners/Miners";
 
 function HomePage() {
@@ -10,6 +11,7 @@ function HomePage() {
             <Brand />
             <Miners />
             <Experts />
+            <Hosting />
         </div>
     );
 }
---
+++ components/hosting/Hosting.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import image48 from "../../public/Img/expertsImg/image48.png";
+import Title from "../title/Title";
+
+const Hosting = () => {
+    return (
+        <section className="min-h-[895px] bg-[#F4F8FB]">
+            <div className="mx-60">
+                <Title
+                    data="HOSTING PARTNER"
+                    mt={"pt-[90px]"}
+                    mb={"pb-[104px]"}
+                />
+                <div className="flex gap-[75px]">
+                    <div className="w-1/2">
+                        <Image className="h-[550px] w-[716px]" src={image48} alt="image" />
+                    </div>
+                    <div className="w-1/2">
+                        <div className="w-[649px]">
+                            <p className="text-[96px] leading-[114px] font-bold text-[#102542]pb-[42px]">
+                                <span className="text-[#F45D02]">CRYPTO</span>  GURU DEALS</p>
+                            <p className="w-[649px] text-[20px] leading-[30px] pt-[42px]">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hosting;
