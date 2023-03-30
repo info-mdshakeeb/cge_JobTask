c8c9dcfdea4de938365a87e31769a75b33f1a8cd
itzshakeeb <itzshakeeb@gmail.com>
2023-03-30 11:46:13 +0600

    responsive  blog section

--- components/blog/Blog.js
+++ components/blog/Blog.js
@@ -15,14 +15,14 @@ const Blog = () => {
                     mb={"pb-[78px]"}
                 />
             </div>
-            <div className="flex gap-[10px] 2xl:gap-[20px] ">
-                <div className="h-[550px] w-[462px] border border-[#FFD33F] rounded-[21px]">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-[10px] 2xl:gap-[20px]">
+                <div className="2xl:h-[550px] 2xl:w-[462px] border border-[#FFD33F] rounded-[21px]">
                     <div className="mt-[36px] px-[29px] ">
-                        <Image src={CartImg1} alt="img" />
+                        <Image src={CartImg1} alt="img" className="w-full " />
                         <div className="mt-[25px] mb-[15px]">
                             <p className="text-[15px] font-[400] text-[#808682]">February 9, 2021</p>
                         </div>
-                        <div className="w-[400px] text-[20px] text-[#180D5B] mb-[15px]">
+                        <div className="max-w-[400px]  text-[20px] text-[#180D5B] mb-[15px]">
                             <p>Top Cryptocurrency Mining Hardware In 2021</p>
                         </div>
                         <div className="mb-[19px] text-[20px]">
@@ -33,36 +33,36 @@ const Blog = () => {
                         </div>
                     </div>
                 </div>
-                <div className="h-[550px] w-[462px] border border-[#FFD33F] rounded-[21px]">
+                <div className="2xl:h-[550px] 2xl:w-[462px] border border-[#FFD33F] rounded-[21px]">
                     <div className="mt-[36px] px-[29px] ">
-                        <Image src={CartImg1} alt="img" />
+                        <Image src={CartImg1} alt="img" className="w-full " />
                         <div className="mt-[25px] mb-[15px]">
                             <p className="text-[15px] font-[400] text-[#808682]">February 9, 2021</p>
                         </div>
-                        <div className="w-[400px] text-[20px] text-[#180D5B] mb-[15px]">
+                        <div className="max-w-[400px]  text-[20px] text-[#180D5B] mb-[15px]">
                             <p>Top Cryptocurrency Mining Hardware In 2021</p>
                         </div>
                         <div className="mb-[19px] text-[20px]">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
                         </div>
-                        <div className="w-full">
+                        <div className="w-full pb-[27px]">
                             <button className="btn btn-ghost border-[#FFD33F] rounded-[100px] w-full "> Read More</button>
                         </div>
                     </div>
                 </div>
-                <div className="h-[550px] w-[462px] border border-[#FFD33F] rounded-[21px]">
+                <div className="2xl:h-[550px] 2xl:w-[462px] border border-[#FFD33F] rounded-[21px]">
                     <div className="mt-[36px] px-[29px] ">
-                        <Image src={CartImg1} alt="img" />
+                        <Image src={CartImg1} alt="img" className="w-full " />
                         <div className="mt-[25px] mb-[15px]">
                             <p className="text-[15px] font-[400] text-[#808682]">February 9, 2021</p>
                         </div>
-                        <div className="w-[400px] text-[20px] text-[#180D5B] mb-[15px]">
+                        <div className="max-w-[400px] text-[20px] text-[#180D5B] mb-[15px]">
                             <p>Top Cryptocurrency Mining Hardware In 2021</p>
                         </div>
                         <div className="mb-[19px] text-[20px]">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
                         </div>
-                        <div className="w-full">
+                        <div className="w-full pb-[27px]">
                             <button className="btn btn-ghost border-[#FFD33F] rounded-[100px] w-full "> Read More</button>
                         </div>
                     </div>
--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -70,9 +70,6 @@ const Footer = () => {
                     </footer>
                 </div>
             </div>
-
-
-
         </section>
     );
 };
